06908c20a7b3d31e16d3507bafe9e037041b1256
Mohit Mishra <immadmohit@gmail.com>
2026-01-31 20:40:11 +0530

    remove useless scripts

--- frontend/sentry.server.config.ts
+++ frontend/sentry.server.config.ts
@@ -1,7 +1,19 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
 import * as Sentry from "@sentry/nextjs";
 
 Sentry.init({
-  dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
-  tracesSampleRate: 1.0,
-  debug: false,
+  dsn: "https://8521fea573d8112b16b51e0610ac2bcb@o4510784958955520.ingest.us.sentry.io/4510784982351872",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Enable logs to be sent to Sentry
+  enableLogs: true,
+
+  // Enable sending user PII (Personally Identifiable Information)
+  // https://docs.sentry.io/platforms/javascript/guides/nextjs/configuration/options/#sendDefaultPii
+  sendDefaultPii: true,
 });
